2eece58b32f947fe0c984a711576704840ff2849
felipecalvo239@gmail.com <felipecalvo239@gmail.com>
2018-02-25 09:00:06 -0500

    Mumuki Export on 2018-02-25 09:00:06 -0500

---
+++ 00009_Planta/test.java
@@ -0,0 +1,30 @@
+Planta planta = new Planta();
+
+@Test
+public void una_planta_da_frutos() {
+  Assert.assertTrue(planta.daFrutos());
+}
+
+@Test
+public void una_planta_aumenta_5_hojas_al_crecer() {
+  planta.crecer();
+  Assert.assertEquals(5, planta.cantidadDeHojas());
+  planta.crecer();
+  Assert.assertEquals(10, planta.cantidadDeHojas());
+}
+
+@Test
+public void una_planta_hace_fotosintesis() {
+  planta.crecer();
+  planta.crecer();
+  Assert.assertEquals(20, planta.fotosintesis());
+}
+
+@Test
+public void una_planta_se_marchita() {
+  planta.crecer();
+  planta.crecer();
+  planta.crecer();
+  planta.marchitarse();
+  Assert.assertEquals(0, planta.cantidadDeHojas());
+}
