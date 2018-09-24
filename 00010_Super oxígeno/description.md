e551970c14d6e957d68168a2eb220ada249ba013
gus.trucco@gmail.com <gus.trucco@gmail.com>
2018-09-24 14:36:09 -0400

    Mumuki Export on 2018-09-24 14:36:09 -0400

--- 00010_Super oxígeno/description.md
+++ 00010_Super oxígeno/description.md
@@ -1,6 +1,6 @@
-¡Las plantas han evolucionado! :eyes: Al hacer `fotosintesis`, ahora liberan 100 puntos más de oxígeno que antes.
+¡Los arbustos han evolucionado! :eyes: Al hacer `fotosintesis`, ahora liberan 100 puntos más de oxígeno que antes.
 
-¡Esto nos pone en un punto medio! Ahora las plantas sólo coinciden en **parte** de la lógica de `fotosintesis`. Si bien suman 100 al cálculo original, siguen comportándose igual que los árboles al usar la cantidad de hojas en su cálculo. Recordemos el uso de **super**:
+¡Esto nos pone en un punto medio! Ahora los arbustos sólo coinciden en **parte** de la lógica de `fotosintesis`. Si bien suman 100 al cálculo original, siguen comportándose igual que los árboles al usar la cantidad de hojas en su cálculo. Recordemos el uso de **super**:
 
 ```java
 class Despedida {
@@ -27,4 +27,4 @@ Esto significa que al realizar las siguientes consultas...
 
 **super** permite reutilizar parte de la lógica de la superclase a la vez que se agrega comportamiento propio de la subclase.
 
-> ¡Último uso de herencia! Agregá el método `fotosintesis` a `Planta` sin repetir lógica con `Arbol`.
+> ¡Último uso de herencia! Agregá el método `fotosintesis` a `Arbusto` sin repetir lógica con `Arbol`.
--- 00010_Super oxígeno/test.java
+++ 00010_Super oxígeno/test.java
@@ -1,30 +1,30 @@
-Planta planta = new Planta();
+Arbusto arbusto = new Arbusto();
 
 @Test
-public void una_planta_da_frutos() {
-  Assert.assertTrue(planta.daFrutos());
+public void un_arbusto_da_frutos() {
+  Assert.assertTrue(arbusto.daFrutos());
 }
 
 @Test
-public void una_planta_aumenta_5_hojas_al_crecer() {
-  planta.crecer();
-  Assert.assertEquals(5, planta.getCantidadDeHojas());
-  planta.crecer();
-  Assert.assertEquals(10, planta.getCantidadDeHojas());
+public void un_arbusto_aumenta_5_hojas_al_crecer() {
+  arbusto.crecer();
+  Assert.assertEquals(5, arbusto.getCantidadDeHojas());
+  arbusto.crecer();
+  Assert.assertEquals(10, arbusto.getCantidadDeHojas());
 }
 
 @Test
-public void una_planta_hace_fotosintesis() {
-  planta.crecer();
-  planta.crecer();
-  Assert.assertEquals(120, planta.fotosintesis());
+public void un_arbusto_hace_fotosintesis() {
+  arbusto.crecer();
+  arbusto.crecer();
+  Assert.assertEquals(120, arbusto.fotosintesis());
 }
 
 @Test
-public void una_planta_se_marchita() {
-  planta.crecer();
-  planta.crecer();
-  planta.crecer();
-  planta.marchitarse();
-  Assert.assertEquals(0, planta.getCantidadDeHojas());
+public void una_arbusto_se_marchita() {
+  arbusto.crecer();
+  arbusto.crecer();
+  arbusto.crecer();
+  arbusto.marchitarse();
+  Assert.assertEquals(0, arbusto.getCantidadDeHojas());
 }
